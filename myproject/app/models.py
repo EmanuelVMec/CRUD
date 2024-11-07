2222dda415ab5184198aa969f58310d4fd498a9b
EmanuelVMec <joshua.vinces5503@utc.edu.ec>
2024-11-07 09:10:22 -0500

    posi

--- myproject/app/models.py
+++ myproject/app/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Producto (models.Model):
-    nombre = models.CharField(max_length=500, blank= True)
-    descripcion = models.TextField(max_length=2000, blank=True)
-    precio = models.DecimalField(max_digits=4, decimal_places=2)
+    nombre = models.CharField(max_length=100, blank=False, verbose_name="Nombre : " , help_text="introdusca solo valores de caracteres",editable=True)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
     fecha_de_ingreso = models.DateTimeField(auto_now_add=True)
 
     def _str_ (self):
